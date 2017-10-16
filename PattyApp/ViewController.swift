8542bef42cc2d109514cb5ef9b88efefaad58d0b
Burak Nurçiçek <burak.nurcicek@ozu.edu.tr>
2017-10-17 00:13:47 +0300

    Mainstoryboard added

--- PattyApp/ViewController.swift
+++ PattyApp/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
+    
+    var Player: AVPlayer!
+    var PlayerLayer: AVPlayerLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        let URL = Bundle.main.url(forResource: "dog1", withExtension: "mp4")
+        Player = AVPlayer.init(url: URL!)
+        
+        
+        PlayerLayer = AVPlayerLayer(player: Player)
+        PlayerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        PlayerLayer.frame = view.layer.frame
+        
+        Player.actionAtItemEnd = AVPlayerActionAtItemEnd.none
+    //    view.sizeToFit()
+        Player.isMuted = true
+
+        Player.play()
+        
+        view.layer.insertSublayer(PlayerLayer, at: 0)
+        NotificationCenter.default.addObserver(self, selector: #selector(playerItemReachEnd(notification:)), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: Player.currentItem)
+    }
+    func playerItemReachEnd(notification: NSNotification) {
+        
+        Player.seek(to: kCMTimeZero)
+        
     }
 
     override func didReceiveMemoryWarning() {
