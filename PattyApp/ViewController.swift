4da6317d39bd065811cf3a28509d09674cddb65c
dilarakorkmazgit <dilarakorkmaz@ik-mb-air.local>
2017-10-16 14:03:03 +0300

    Initial Commit

---
+++ PattyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PattyApp
+//
+//  Created by Dilara Korkmaz on 16/10/2017.
+//  Copyright © 2017 Dilara Korkmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
