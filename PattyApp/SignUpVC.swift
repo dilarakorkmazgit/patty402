14f60c64b330f8a95c2c74a2aafd7071f36b57bc
Burak Nurçiçek <burak.nurcicek@ozu.edu.tr>
2017-10-19 00:22:37 +0300

    register vc added

---
+++ PattyApp/SignUpVC.swift
@@ -0,0 +1,43 @@
+//
+//  CreateAccountViewController.swift
+//  PattyApp
+//
+//  Created by Dilara Korkmaz on 18/10/2017.
+//  Copyright © 2017 Dilara Korkmaz. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func signUpPressed(_ sender: Any) {
+        
+        self.performSegue(withIdentifier: "LoggedInVC", sender: nil)
+        
+    }
+
+
+
+
+
+
+}
