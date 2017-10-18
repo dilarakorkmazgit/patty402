31e974492b2336048a67bda6c709de4cfd03ae7e
dilarakorkmazgit <dilarakorkmaz@ik-mb-air.local>
2017-10-18 16:33:00 +0300

    ScrollViewwithPageControl

--- PattyApp/ViewController.swift
+++ PattyApp/ViewController.swift
@@ -11,17 +11,40 @@ import AVFoundation
 import MapKit
 
 
+class ViewController: UIViewController,UIScrollViewDelegate {
+    
+    @IBOutlet weak var ScrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    var colorRange :[UIColor] = [UIColor.red,UIColor.yellow,UIColor.blue,UIColor.purple]
 
-
-class ViewController: UIViewController {
+    var frame = CGRect(x:0,y:0,width:0,height:0)
+    
     
     var Player: AVPlayer!
     var PlayerLayer: AVPlayerLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-       
+        
+        //Do any additional setup after loading the view, typically from a nib.
+        
+        //Adding different scrolview Page
+        
+        pageControl.numberOfPages = colorRange.count
+        for index in 0..<colorRange.count {
+         
+            frame.origin.x = ScrollView.frame.size.width * CGFloat(index)
+            frame.size = ScrollView.frame.size
+            
+            let view = UIView(frame:frame)
+            view.backgroundColor = colorRange[index]
+            self.ScrollView.addSubview(view)
+        }
+        
+        ScrollView.contentSize = CGSize(width:(ScrollView.frame.size.width * CGFloat(colorRange.count)),height :ScrollView.frame.size.height)
+        
+        ScrollView.delegate = self
+        
         
         //Get video from the Firebase Strogae URL
         let URL:NSURL = NSURL(string: "https://firebasestorage.googleapis.com/v0/b/pattyapp-34c16.appspot.com/o/dog1.mp4?alt=media&token=98ab3c41-c645-4535-a70b-41511b5df602")!
@@ -36,12 +59,20 @@ class ViewController: UIViewController {
        
         //view.sizeToFit()
         Player.isMuted = true
-
         Player.play()
         
         view.layer.insertSublayer(PlayerLayer, at: 0)
         NotificationCenter.default.addObserver(self, selector: #selector(playerItemReachEnd(notification:)), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: Player.currentItem)
     }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView){
+        
+        let pageNumber = ScrollView.contentOffset.x / ScrollView.frame.size.width
+        pageControl.currentPage = Int(pageNumber)
+        pageControl.currentPageIndicatorTintColor = colorRange[pageControl.currentPage]
+        
+    }
+    
     func playerItemReachEnd(notification: NSNotification) {
         
         Player.seek(to: kCMTimeZero)
@@ -53,6 +84,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pageChanger(_ sender: UIPageControl) {
+    
+        let x = CGFloat(sender.currentPage) * ScrollView.frame.size.width
+        ScrollView.contentOffset = CGPoint(x:x,y:0)
+    }
 
 }
 
