331bdb509a549ac992f7110ead91a06e9c58fc15
dilarakorkmazgit <dilarakorkmaz@ik-mb-air.local>
2017-10-18 17:52:03 +0300

    CreateAccountViewController

---
+++ PattyApp/CreateAccountViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CreateAccountViewController.swift
+//  PattyApp
+//
+//  Created by Dilara Korkmaz on 18/10/2017.
+//  Copyright © 2017 Dilara Korkmaz. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+
+
+
+
+}
