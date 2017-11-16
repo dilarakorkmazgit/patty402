24bffa6b113e6e907d9f325ae95c2cedb83dc7d1
Burak Nurçiçek <burak.nurcicek@ozu.edu.tr>
2017-11-16 21:38:27 +0300

    storyboard updated, picker view, camera and library added

--- PattyApp/LoggedInVC.swift
+++ PattyApp/LoggedInVC.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class LoggedInVC: UIViewController {
+class LoggedInVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var cinsiyetButton: UIButton!
+    
+    @IBOutlet weak var cinsiyetPicker: UIPickerView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    
+       let cinsiyet = ["Erkek","Dişi"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        super.viewDidLoad()
+        cinsiyetPicker.dataSource = self
+        cinsiyetPicker.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +36,66 @@ class LoggedInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func cinsiyetPressed(_ sender: Any) {
+        cinsiyetPicker.isHidden = false
+        
+    }
 
+    @IBAction func chooseImage(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }else {
+                print ("Camera not available")
+            }
+            
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        picker.dismiss(animated: true, completion: nil)
+        
+        
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return cinsiyet.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return cinsiyet[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        cinsiyetButton.setTitle(cinsiyet[row], for: UIControlState())
+        cinsiyetPicker.isHidden = true
+    }
+    
 
 }
