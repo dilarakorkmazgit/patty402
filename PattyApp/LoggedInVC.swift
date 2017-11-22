e3c6674c3cb97a7dcea00c0499c94b6a9b738166
dilarakorkmazgit <dilarakorkmaz@ik-mb-air.local>
2017-11-22 18:42:32 +0300

    User pet information- Firebase database updated

--- PattyApp/LoggedInVC.swift
+++ PattyApp/LoggedInVC.swift
@@ -6,19 +6,48 @@
 //  Copyright © 2017 Dilara Korkmaz. All rights reserved.
 //
 
+import Foundation
+import Firebase
 import UIKit
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
 
 class LoggedInVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
     
+    var ref: DatabaseReference!
+
+    @IBOutlet weak var petNameLabel: UITextField!
+    
+    //butonlar
     @IBOutlet weak var cinsiyetButton: UIButton!
     
-    @IBOutlet weak var cinsiyetPicker: UIPickerView!
+    @IBOutlet weak var türButton: UIButton!
     
+    @IBOutlet weak var ageButton: UIButton!
+    
+    @IBOutlet weak var colorBotton: UIButton!
+    
+    @IBOutlet weak var healthButton: UIButton!
+    
+    //pickerlar
+    @IBOutlet weak var cinsiyetPicker: UIPickerView!
+    @IBOutlet weak var turPicher: UIPickerView!
+    @IBOutlet weak var agePicker: UIPickerView!
+    @IBOutlet weak var colorPicker: UIPickerView!
+    @IBOutlet weak var healthPicker: UIPickerView!
+    
+    //pet resmi
     @IBOutlet weak var imageView: UIImageView!
     
     
      let cinsiyet = ["Erkek","Dişi"]
+    let tur = ["pug","terrier","dogo","doberman"]
+     let color = ["siyah","beyaz","kahverangi","karışık"]
+    let age = ["1","2","3","4","5"]
+    let health = ["kısır","Kısır Değil"]
+
+
     
     
     override func viewDidLoad() {
@@ -26,16 +55,60 @@ class LoggedInVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
         super.viewDidLoad()
         cinsiyetPicker.dataSource = self
         cinsiyetPicker.delegate = self
+        
+        turPicher.dataSource = self
+        turPicher.delegate = self
+        
+        agePicker.dataSource = self
+        agePicker.delegate = self
+        
+        colorPicker.dataSource = self
+        colorPicker.delegate = self
+        
+        healthPicker.dataSource = self
+        healthPicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    //when pressed the button,show picker
     @IBAction func cinsiyetPressed(_ sender: Any) {
         cinsiyetPicker.isHidden = false
         
     }
+    
+    
+    @IBAction func turPressed(_ sender: Any) {
+        turPicher.isHidden = false
+
+    }
+    
+    @IBAction func agePressed(_ sender: Any) {
+    
+        agePicker.isHidden = false
+
+    }
+    
+    
+    
+    @IBAction func colorPressed(_ sender: Any) {
+        
+        colorPicker.isHidden = false
+
+    }
+    
+    @IBAction func healthPressed(_ sender: Any) {
+        healthPicker.isHidden = false
+
+    }
+    
+    
+    
+    
+    
+    
 
     @IBAction func chooseImage(_ sender: Any) {
         let imagePickerController = UIImagePickerController()
@@ -81,17 +154,94 @@ class LoggedInVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return cinsiyet.count
-    }
-    
+        if pickerView.tag==1{
+            return cinsiyet.count
+
+        }
+        else if pickerView.tag==2{
+            return tur.count
+        }
+        else if pickerView.tag==3{
+            return age.count
+        }
+        else if pickerView.tag==4{
+            return color.count
+        }
+        else if pickerView.tag==5 {
+            return health.count
+        }
+        return health.count
+}
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return cinsiyet[row]
-    }
+        
+        
+        if pickerView.tag==1{
+            return cinsiyet[row]
+            
+        }
+        else if pickerView.tag==2{
+            return tur[row]
+        }
+        else if pickerView.tag==3{
+            return age[row]
+        }
+        else if pickerView.tag==4{
+            return color[row]
+        }
+        else if pickerView.tag==5 {
+            return health[row]
+        }
+        return health[row]
+
+        }
+    
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        if pickerView.tag==1{
         cinsiyetButton.setTitle(cinsiyet[row], for: UIControlState())
         cinsiyetPicker.isHidden = true
     }
+        if pickerView.tag==2{
+            türButton.setTitle(tur[row], for: UIControlState())
+            turPicher.isHidden = true
+        }
+        if pickerView.tag==3{
+            ageButton.setTitle(age[row], for: UIControlState())
+            agePicker.isHidden = true
+        }
+        if pickerView.tag==4{
+            colorBotton.setTitle(color[row], for: UIControlState())
+            colorPicker.isHidden = true
+        }
+        if pickerView.tag==5{
+            healthButton.setTitle(health[row], for: UIControlState())
+            healthPicker.isHidden = true
+        }
+    }
     
+    @IBAction func kaydetPressed(_ sender: Any) {
+    
+        let uid = Auth.auth().currentUser?.uid
+        
+
+            let petName: String = self.petNameLabel.text!
+            let gender: String = self.cinsiyetButton.currentTitle!
+            let age: String = self.ageButton.currentTitle!
+            let tur: String = self.türButton.currentTitle!
+            let health: String = self.healthButton.currentTitle!
+            let color: String = self.colorBotton.currentTitle!
+            
+        Database.database().reference().child(uid!).child("pet").setValue(["petname": petName, "gender": gender, "age": age, "tür":tur, "color": color,"health": health])
+
+            //self.ref.child(userID).childByAutoId().setValue(["petname": petName, "gender": gender, "age": age, "tür":tur, "color": color,"health": health])
+
+    
+         //   print("user registered66 " + user)
+        
+        
+     //   self.performSegue(withIdentifier: "Home", sender: nil)
+     
+    }
 
 }
