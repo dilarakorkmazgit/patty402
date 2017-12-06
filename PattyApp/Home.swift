c8c824c0087a6c2d690c85fb966e814cae870097
Burak Nurçiçek <burak.nurcicek@ozu.edu.tr>
2017-12-07 00:10:03 +0300

    loading from firebase added

--- PattyApp/Home.swift
+++ PattyApp/Home.swift
@@ -9,32 +9,50 @@
 import UIKit
 import MapKit
 import CoreLocation
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
 
 class Home: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
-
+    
+    @IBOutlet weak var imageViewer: UIImageView!
     @IBOutlet weak var mapView: MKMapView!
     var dog = MKPointAnnotation()
     
-   // let manager = CLLocationManager()
     
-    //func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-      //  let location = locations [0]
-        
-       // let span:MKCoordinateSpan = MKCoordinateSpanMake()
-        //let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
-        
-      //  let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation,span)
-     //   map.setRegion(region, animated: true)
-        
-       // self.map.showsUserLocation = true
-    //}
+    
+    // KONUM SERVİSLERİ ETKİNLEŞTİRME
+    /* let manager = CLLocationManager()
+     
+     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+     let location = locations [0]
+     
+     let span:MKCoordinateSpan = MKCoordinateSpanMake()
+     let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+     
+     let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation,span)
+     map.setRegion(region, animated: true)
+     
+     self.map.showsUserLocation = true
+     }*/
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //manager.delegate = self
-        //manager.desiredAccuracy = kCLLocationAccuracyBest
-        //manager.requestWhenInUseAuthorization()
-        //manager.startUpdatingLocation()
+        
+        let database = Database.database().reference()
+        let storage = Storage.storage().reference()
+        let mango = storage.child("profileImage/mango.png")
+        
+        mango.getData(maxSize: 1*1000*2000) { (data, error) in
+            if error == nil {
+                self.imageViewer.image = UIImage(data: data!)
+            }
+        }
+    
+        /* manager.delegate = self
+         manager.desiredAccuracy = kCLLocationAccuracyBest
+         manager.requestWhenInUseAuthorization()
+         manager.startUpdatingLocation()*/
         
         self.mapView.delegate = self
         mapView.showsUserLocation = true
@@ -48,6 +66,8 @@ class Home: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        
         if !(annotation is MKPointAnnotation) {
             print("Not registered")
             return nil
@@ -63,8 +83,9 @@ class Home: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
         annotationView!.image = UIImage(named: "dog2.png")
         return annotationView
         
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
