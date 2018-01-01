a2dc2bc61779d09ee58823385d7ba7303ed7360d
Burak Nurçiçek <burak.nurcicek@ozu.edu.tr>
2018-01-02 01:17:40 +0300

    bug fixed

--- PattyApp/ChatInfo.swift
+++ PattyApp/ChatInfo.swift
@@ -92,7 +92,14 @@ class ChatInfo: UITableViewController {
         override func layoutSubviews() {
             
             super.layoutSubviews()
-            
+          
+            textLabel?.frame = CGRectMake(56, textLabel!.frame.origin.y, textLabel!.frame.width, textLabel!.frame.height)
+            detailTextLabel?.frame = CGRectMake(56, detailTextLabel!.frame.origin.y, detailTextLabel!.frame.width, textLabel!.frame.height)
+       
+        }
+        
+        func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+            return CGRect(x: x, y: y, width: width, height: height)
         }
         
         let profileImageView: UIImageView = {
