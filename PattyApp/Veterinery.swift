048d946ac5906feed541f6d42716c5563f0584f5
Burak Nurçiçek <burak.nurcicek@ozu.edu.tr>
2018-01-15 20:10:37 +0300

    vet added

---
+++ PattyApp/Veterinery.swift
@@ -0,0 +1,18 @@
+//
+//  Veterinery.swift
+//  PattyApp
+//
+//  Created by Burak Nurçiçek on 15.01.2018.
+//  Copyright © 2018 Dilara Korkmaz. All rights reserved.
+//
+
+import UIKit
+
+class Veterinery: NSObject {
+
+    var address : String?
+    var calender : String?
+    var phone : Int?
+    var photo : String?
+    var Name: String?
+}
--- PattyApp/VeterineryViewController.swift
+++ PattyApp/VeterineryViewController.swift
@@ -8,30 +8,97 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
-class VeterineryViewController: RootViewController {
-
+class VeterineryViewController: RootViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var VetNameLabel: UILabel!
+    
+    @IBOutlet weak var TableViewForVet: UITableView!
+    
+    
+    let cellVet = "cellVet"
+    
+    var vet = [Veterinery]()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        TableViewForVet.register(UserCell.self, forCellReuseIdentifier: cellVet)
+        
+        self.title = "Veteriner"
+        self.TableViewForVet.delegate = self
+        self.TableViewForVet.dataSource = self
+        
+        observeAllUser()
         
         
+    }
+    func observeAllUser(){
         
-        //fit the background image
-        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
-        backgroundImage.image = UIImage(named: "background-profil")
-        backgroundImage.contentMode =  UIViewContentMode.scaleAspectFill
-        self.view.insertSubview(backgroundImage, at: 0)
-
-        // Do any additional setup after loading the view.
+        Database.database().reference().child("Veterinery").observe(.childAdded, with: {(snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                
+                let vet = Veterinery()
+                
+                vet.address = dictionary["Address"] as? String
+                //vet.calender = dictionary["Calender"] as? String
+                vet.Name = dictionary["Name"] as? String
+                vet.phone = dictionary["Phone"] as? Int
+                vet.photo = dictionary["Photo"] as? String
+                print(snapshot)
+                
+                self.vet.append(vet)
+                
+                DispatchQueue.main.async(execute: {
+                    self.TableViewForVet.reloadData()
+                })
+            }
+            
+        } , withCancel: nil)
     }
     
     
-    
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        
+        return vet.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = TableViewForVet.dequeueReusableCell(withIdentifier: cellVet, for: indexPath) as! UserCell
 
+        
+        let vet = self.vet[indexPath.row]
+        
+        cell.textLabel?.text = vet.Name
+        cell.detailTextLabel?.text = vet.address
+       
+        if let profileImageUrl = vet.photo {
+            
+            cell.profileImageView.loadImageUsingCacheWithUrlString(urlString: profileImageUrl)
+        }
+        return cell
+    }
+    
+    
 }
