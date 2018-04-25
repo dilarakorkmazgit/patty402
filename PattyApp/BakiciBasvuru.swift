f6c8b1a7159c0c96d1b2a573aa8ea9a3fdca4d03
Burak Nurcicek <buraknurcicek@Burak-Mac.local>
2018-04-25 07:35:56 -0700

    comit

---
+++ PattyApp/BakiciBasvuru.swift
@@ -0,0 +1,45 @@
+//
+//  BakıcıBasvuru.swift
+//  PattyApp
+//
+//  Created by Burak Nurcicek on 7.04.2018.
+//  Copyright © 2018 Dilara Korkmaz. All rights reserved.
+//
+
+import UIKit
+
+class BakiciBasvuru: RootViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    let kediBakıcı = ["Günlük", "Yatılı", "Uzun Süreli"]
+    let kopekBakıcı = ["Günlük", "Yatılı", "Uzun Süreli","Gezdirme","Küçük Tür", "Büyük Tür"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kediBakıcı.count
+    }
+    
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = kediBakıcı[indexPath.row]
+        return cell
+    }
+      func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCellAccessoryType.checkmark
+       {
+        tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCellAccessoryType.none
+        }
+    }
+
+}
