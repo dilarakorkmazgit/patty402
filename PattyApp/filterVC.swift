b8172c7789ed00df49cbdb9e1650497459d16f27
Burak Nurcicek <buraknurcicek@Burak-Mac.local>
2018-04-27 03:07:24 -0700

    tableView updated

--- PattyApp/filterVC.swift
+++ PattyApp/filterVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class filterVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -15,11 +16,12 @@ class filterVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView_3: UITableView!
     
     let cinsiyet =  ["Dişi","Erkek"]
-    var count = 2
-    var count2 = 3
-    var count3 = 4
+    let tur = ["Pug","Terrier","Dogo","Doberman","Rottwailer","Beagle","Buldog","Danua","Kaniş","Chihuahua","Boxor","Cow-cow","Pitbull","Akita","Pomeranian","Bulmastif"]
+    let age = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]
     
     
+    var pet = [Pet]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,7 +33,7 @@ class filterVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         
         self.tableView_3.delegate = self
         self.tableView_3.dataSource = self
-        
+        observeGender()
         
     }
     
@@ -42,35 +44,58 @@ class filterVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
+     
         if (tableView == self.tableView_1) {
             var cell: cinsiyetTableView = tableView.dequeueReusableCell(withIdentifier: "cellTableView1") as! cinsiyetTableView
+
+            cell.labelText1.text = cinsiyet[indexPath.row]
             return cell
         }
         else if(tableView == self.tableView_2) {
             var cell: turTableView = tableView.dequeueReusableCell(withIdentifier: "cellTableView2") as! turTableView
-            cell.labelText2.text = "b \(indexPath.row) "
+            cell.labelText2.text =  tur[indexPath.row]
             return cell
         }
         else {
             var cell: yasTableView = tableView.dequeueReusableCell(withIdentifier: "cellTableView3") as! yasTableView
-            cell.labelText3.text = "c \(indexPath.row) "
+            cell.labelText3.text =  age[indexPath.row]
             return cell
             
         }
         
     }
+    func observeGender(){
+        
+        Database.database().reference().child("user").child("pet").observe(.childAdded, with: {(snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                
+                let pet = Pet()
+                
+                pet.gender = dictionary["gender"] as? String
+              
+                
+                self.pet.append(pet)
+                
+                DispatchQueue.main.async(execute: {
+                    self.tableView_1.reloadData()
+                })
+            }
+            
+        } , withCancel: nil)
+    }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if  (tableView == self.tableView_1) {
-            return count
+            return cinsiyet.count
         }else if(tableView == self.tableView_2) {
-            return count2
+            return tur.count
             
         }else if(tableView == self.tableView_3){
-            return count3
+            return age.count
         }
-        return count
+        return 0
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
